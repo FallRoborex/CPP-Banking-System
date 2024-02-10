7060f9c201471ad033bb895e7f39a02abcd675bb
FallRoborex <robertoramirezgalan04@gmail.com>
2024-02-10 13:13:30 -0500

    Created a display method and a way to test out the class

--- src/Account.cpp
+++ src/Account.cpp
@@ -2,7 +2,7 @@
 
 class Account
 {
-    int acount_number;
+    int account_number;
     char name[50];
     int deposit;
     char type;
@@ -16,23 +16,31 @@ public:
 
 void Account::createAccount()
 {
-    cout <<"\n Enter an acount number: ";
-    cin >> acount_number;
-    cout << "\n Enter the name of the acount holder only 50 characters allowed: ";
+    cout <<"\n Enter an account number: ";
+    cin >> account_number;
+    cout << "\n Enter the name of the account holder only 50 characters allowed: ";
     cin.ignore();
     cin.getline(name, 50);
     cout << "\n Enter Type of account checking or savings. (C/S): ";
     cin >> type;
     type = toupper(type);
-    cout << "Enter the Initial amount of money you want to start the account with. (Saving => 5) (Checking => 10)";
+    cout << "Enter the Initial amount of money you want to start the account with. (Saving => 5) (Checking => 10): ";
     cin >> deposit;
     cout << "The account have been created";
+}
 
+void Account::displayAccount() {
+    cout << "Account number:" << account_number << endl;
+    cout << "Account Name:" << name << endl;
+    cout << "Account type: " << name << endl;
 }
 
 
+
+
 int main() {
-    cout << "Testing";
-    
+    Account person;
+    person.createAccount();
+    person.displayAccount();
     return 0;
 }
