03ad09cb6238a2902cb4e7c8d1b26bc5b3947862
FallRoborex <robertoramirezgalan04@gmail.com>
2024-02-10 12:34:14 -0500

    Made an Account class with a createAccount method

--- src/Account.cpp
+++ src/Account.cpp
@@ -13,3 +13,26 @@ public:
     void displayAccount();
     
 };
+
+void Account::createAccount()
+{
+    cout <<"\n Enter an acount number: ";
+    cin >> acount_number;
+    cout << "\n Enter the name of the acount holder only 50 characters allowed: ";
+    cin.ignore();
+    cin.getline(name, 50);
+    cout << "\n Enter Type of account checking or savings. (C/S): ";
+    cin >> type;
+    type = toupper(type);
+    cout << "Enter the Initial amount of money you want to start the account with. (Saving => 5) (Checking => 10)";
+    cin >> deposit;
+    cout << "The account have been created";
+
+}
+
+
+int main() {
+    cout << "Testing";
+    
+    return 0;
+}
